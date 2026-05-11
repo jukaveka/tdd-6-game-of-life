9d0d566f8c9b8ff6dd45fbbb9a24fd56ce997e65
jukaveka <juhokavekari@hotmail.com>
2026-05-12 01:12:32 +0300

    Random format

--- test/Cell.test.mjs
+++ test/Cell.test.mjs
@@ -7,49 +7,49 @@ describe("Cell", () => {
     const cell = new Cell(true);
 
     expect(cell).to.be.a("object");
-  })
+  });
 
   test("has populated value", () => {
     const cell = new Cell(true);
 
     expect(cell).to.have.key("populated");
-  })
+  });
 
   test("can depopulate", () => {
     const cell = new Cell(true);
     cell.depopulate();
 
     expect(cell.populated).to.equal(false);
-  })
+  });
 
   test("can populate", () => {
     const cell = new Cell(false);
     cell.populate();
 
     expect(cell.populated).to.equal(true);
-  })
+  });
 
   test("can be converted to populated RLE character", () => {
     const cell = new Cell(true);
 
     expect(cell.tag()).to.equal("o");
-  })
+  });
 
   test("can be converted to depopulated RLE character", () => {
     const cell = new Cell(false);
 
     expect(cell.tag()).to.equal("b");
-  })
+  });
 
   test("can be converted to populated string", () => {
     const cell = new Cell(true);
 
     expect(cell.toString()).to.equal("X");
-  })
+  });
 
   test("can be converted to depopulated string", () => {
     const cell = new Cell(false);
 
     expect(cell.toString()).to.equal(" ");
-  })
-})
+  });
+});
