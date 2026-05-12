075a3883e075e600671e85421066b40de97916ea
jukaveka <juhokavekari@hotmail.com>
2026-05-13 01:53:26 +0300

    Test game initialization

--- src/Game.mjs
+++ src/Game.mjs
@@ -1,12 +1,21 @@
+import { File } from "./File.mjs";
+import { RunLengthEncoder } from "./RunLengthEncoder.mjs";
+import { Generation } from "./Generation.mjs";
+
 export class Game {
-  static initialize(filePath, endGeneration) {
-    return new Game(filePath, endGeneration);
+  static async initialize(filePath, endGeneration) {
+    const file = new File(filePath);
+    const data = await file.read();
+    const rle = new RunLengthEncoder();
+    
+    const cells = rle.decoder(data);
+
+    return new Game(cells, endGeneration);
   }
 
-  constructor(filePath, endGeneration) {
-    this.pattern = filePath;
+  constructor(cells, endGeneration) {
+    this.generation = new Generation(cells, 0);
     this.endGeneration = endGeneration;
-    this.currentGeneration = 0;
   }
 
   tick() {
--- test/Game.test.mjs
+++ test/Game.test.mjs
@@ -5,11 +5,15 @@ import { Game } from "../src/Game.mjs";
 describe("Game", () => {
   let game;
 
-  beforeEach(() => {
-    game = Game.initialize("./patterns/testPattern.rle", 5);
+  beforeEach(async () => {
+    game = await Game.initialize("./patterns/testPattern.rle", 5);
   });
 
   test("returns an object", () => {
     expect(game).to.be.a("object");
   });
+
+  test("creates first generation based on input file", () => {
+    expect(game.generation).to.be.an("object").and.to.have.keys("cells", "number");
+  })
 });
